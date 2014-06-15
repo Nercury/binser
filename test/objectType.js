8606ca5c6922222366fb68e173174ac45a794011
Nerijus Arlauskas <nercury@gmail.com>
2014-06-15 22:14:11 +0300

    Simple Object serialization.

---
+++ test/objectType.js
@@ -0,0 +1,66 @@
+/* 
+ * Copyright (c) Nerijus Arlauskas
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is furnished
+ * to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+/**
+ * Created by Nercury on 2014-06-15.
+ */
+
+var ObjectType = require('../lib/objectType.js');
+var types = require('../lib/types.js');
+
+module.exports = {
+    EmptyObjectType: function (test) {
+        var emptyObjectType = new ObjectType();
+        test.same(
+            {},
+            emptyObjectType.deserialize(emptyObjectType.serialize({}))
+        );
+        test.done();
+    },
+    SimpleObjectType: function (test) {
+
+        // Define object contents.
+        var simpleObjectType = new ObjectType({
+            name : types.String,
+            surname : types.String,
+            age : types.UInt8
+        });
+
+        // Test that it can be serialized and deserialized.
+        test.same(
+            {
+                name: "John",
+                surname: "Unbeatable",
+                age: 12
+            },
+            simpleObjectType.deserialize(simpleObjectType.serialize(
+                {
+                    name: "John",
+                    surname: "Unbeatable",
+                    age: 12
+                }
+            ))
+        );
+        
+        test.done();
+    }
+};
